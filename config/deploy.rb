ed11c6789b3958f0600599da34911221bc8aeac1
Malwine <malwine@mailbox.org>
2016-06-07 16:01:38 +0200

    change port

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '46.101.197.49', port: 80, roles: [:web, :app, :db], primary: true
+server '46.101.197.49', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:Malwine/event_organizer.git'
 set :application,     'event_organizer'
